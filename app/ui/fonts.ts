be1dee5b4ff3e67b85013bb382172f904dcec66e
daniioliver <theartist@damnemail.com>
2024-11-26 04:06:48 -0600

    Updated progress up to lesson 3 tailwind, css, fonts and <Image ...  />

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from 'next/font/google';
+import { Baloo_2 } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const baloo = Baloo_2({ weight: ['400', '700'], subsets: ['latin'] });
+
+
