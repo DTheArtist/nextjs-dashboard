9ea4d3dcfaca0a645c4e63c49585a4687f4d58ef
daniioliver <theartist@damnemail.com>
2024-11-26 04:34:41 -0600

    successfully added temp brand colors to tailwind theme.

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
 		<main className="flex min-h-screen flex-col p-6">
 			<div
 				className={
-					"flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52"
+					"flex h-20 shrink-0 items-end rounded-lg bg-purple-100 p-4 md:h-52"
 				}
 			>
 				<ChivaLogo />
@@ -21,13 +21,13 @@ export default function Page() {
 					<p
 						className={`${baloo.className} antialiased text-xl text-gray-800 md:text-3xl md:leading-normal`}
 					>
-						<strong>Welcome to Chi'Va.</strong> Unlock faster access
+						<strong>Welcome to Chi'Va.</strong><br/>Unlock faster access
 						to behavioral health solutions that reduce delays,
 						improve patient outcomes, and recover lost revenue.{" "}
 					</p>
 					<Link
 						href="/login"
-						className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+						className="flex items-center gap-5 self-start rounded-lg bg-purple-100 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-purple-700 md:text-base"
 					>
 						<span>Request A Demo</span>{" "}
 						<ArrowRightIcon className="w-5 md:w-6" />
@@ -40,7 +40,7 @@ export default function Page() {
 					<p>
 						<a
 							href="https://chivaapp.com"
-							className="text-blue-500"
+							className="text-purple-100"
 						>
 							Alpha Testers Try Chi'Va Now.{" "}
 						</a>
