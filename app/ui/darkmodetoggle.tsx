98381cdfc1826901bac286030c8f8bb75d49bf84
daniioliver <theartist@damnemail.com>
2024-12-07 12:58:35 -0600

    Completed Chapter 9 Suspense and Loading

--- app/ui/darkmodetoggle.tsx
+++ app/ui/darkmodetoggle.tsx
@@ -9,16 +9,17 @@ export default function DarkModeToggle() {
 		<button
 			onClick={toggleDarkMode}
 			className="flex h-[48px] items-center justify-center gap-2 rounded-md p-3 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3
-			hover:bg-violet-600 dark:hover:bg-violet-300
-			bg-slate-700 text-slate-100 
-			dark:bg-slate-200 dark:text-slate-800 "
+			bg-slate-700 text-slate-100 hover:bg-violet-600
+			dark:bg-violet-500 dark:text-slate-100 dark:hover:bg-violet-300 dark:hover:text-slate-800"
 		>
 			{darkMode ? (
 				<SunIcon className="w-5 h-5" />
 			) : (
 				<MoonIcon className="w-5 h-5" />
 			)}
-			{darkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
+			<p className="hidden md:block">
+				{darkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
+			</p>
 		</button>
 	);
 }
