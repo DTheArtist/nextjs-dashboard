5e8ac7a0a4006cc1f54d4aa72415204f75681100
daniioliver <theartist@damnemail.com>
2024-12-07 15:33:31 -0600

    Adding Search completed!

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 
 export default function Page() {
 	return (
-		<main className="flex min-h-screen flex-col p-6">
+		<main className="flex min-h-screen flex-col p-6 bg-slate-200 dark:bg-slate-800 text-slate-800 dark:text-white">
 			<div
 				className={
 					"flex h-20 shrink-0 items-end rounded-lg bg-chivaPurple-500 p-4 md:h-52"
@@ -16,10 +16,10 @@ export default function Page() {
 				<ChivaLogo />
 			</div>
 			<div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-				<div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+				<div className="flex flex-col justify-center gap-6 rounded-lg bg-slate-200 dark:bg-slate-700 px-6 py-10 md:w-2/5 md:px-20">
 					<div className={styles.triangle} />
 					<p
-						className={`${baloo.className} antialiased text-xl text-gray-800 md:text-3xl md:leading-normal`}
+						className={`${baloo.className} antialiased text-xl text-gray-800 dark:text-slate-100 md:text-3xl md:leading-normal`}
 					>
 						<strong>Welcome to Chi'Va.</strong>
 						<br />
@@ -42,9 +42,9 @@ export default function Page() {
 					<p>
 						<a
 							href="https://chivaapp.com"
-							className="text-chivaPurple-500"
+							className="text-chivaPurple-500 dark:text-slate-100 dark:fount-semibold "
 						>
-							Alpha Testers Try Chi'Va Now.{" "}
+							Alpha Testers Try Chi'Va Now. &rarr;.{" "}
 						</a>
 					</p>
 				</div>
@@ -58,7 +58,7 @@ export default function Page() {
 						alt="Screenshots of the dashboard project showing desktop version"
 					/>
 					<Image
-						src="/hero-mobile.png"
+						src="/hero-mobile.jpg"
 						width={560}
 						height={620}
 						className="block md:hidden"
