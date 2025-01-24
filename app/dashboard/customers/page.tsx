9bd4db52094c4be0b4451c89202fbafa9e87c048
Danii Oliver | Quantum Innovation Advisors <114320400+DTheArtist@users.noreply.github.com>
2025-01-24 00:14:59 -0600

    updates to text

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -2,6 +2,7 @@ export default function Page() {
 	return (
 		<main>
 			<p>Customer's Page</p>
+			<p>In submitting this form, you agree to receive information from Chi'Va related to our products, events, and special offers. You can unsubscribe from such messages at any time. We never sell your data, and we value your privacy choices. Please see our Privacy Policy for information.</p>
 		</main>
 	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default function Page() {
 						lost revenue.{" "}
 					</p>
 					<Link
-						href="/login"
+						href="https://chivaapp.com"
 						className="flex items-center gap-5 self-start rounded-lg bg-chivaPurple-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-violet-400 md:text-base"
 					>
 						<span>Request A Demo</span>{" "}
